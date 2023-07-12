a9c83e3bd0f2714bae4571689cf30f1ca43dc870
Simoso68 <87707341+Simoso68@users.noreply.github.com>
2023-07-13 01:05:34 +0200

    made installing prettier

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ if (( $EUID != 0 )); then
     exit
 fi
 
-curl https://raw.githubusercontent.com/Simoso68/i-need-coffee/main/i-need-coffee >> /usr/bin/i-need-coffee
+curl -sS https://raw.githubusercontent.com/Simoso68/i-need-coffee/main/i-need-coffee >> /usr/bin/i-need-coffee
 chmod +x /usr/bin/i-need-coffee
